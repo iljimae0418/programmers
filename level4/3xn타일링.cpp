f4a3577d188961730ace0fffaf5a53b135169bd4
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2018-08-12 03:36:37 -0700

    Update 3xn타일링.cpp

--- level4/3xn타일링.cpp
+++ level4/3xn타일링.cpp
@@ -1,1 +1,20 @@
+#include <string>
+#include <vector>
 
+using namespace std;
+const int mod = 1e9+7; 
+long long f[5005],g[5005]; 
+int solution(int n) {
+    int answer = 0;
+    f[1] = 0;  
+    g[1] = 1;  
+    f[2] = 3;
+    g[2] = 0;
+    for (int i = 3; i <= n; i++){
+        g[i] = (f[i-1]+g[i-2]);  
+        f[i] = (f[i-2]+2*g[i-1]);  
+        g[i] %= mod; 
+        f[i] %= mod; 
+    }
+    return answer = f[n];
+}
